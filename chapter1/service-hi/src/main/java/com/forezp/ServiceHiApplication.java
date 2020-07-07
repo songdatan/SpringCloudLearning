f809b38c3a07a5e0aafc87348e8fec2f2f902a8e
songdatan <tg13213043136@163.com>
2020-07-07 22:05:11 +0800

    test

--- chapter1/service-hi/src/main/java/com/forezp/ServiceHiApplication.java
+++ chapter1/service-hi/src/main/java/com/forezp/ServiceHiApplication.java
@@ -21,6 +21,7 @@ public class ServiceHiApplication {
 	String port;
 	@RequestMapping("/hi")
 	public String home(@RequestParam String name) {
+		System.out.println("111");
 		return "hi "+name+",i am from port:" +port;
 	}
 
